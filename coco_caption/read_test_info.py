678643e52689bcd8fe14043d072776c8eeaf923d
Xinpeng.Chen <2015202130029@whu.edu.cn>
2017-02-13 11:17:14 +0800

    Create read_test_info.py

---
+++ coco_caption/read_test_info.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+import json
+import cPickle as pickle
+
+image_info_test2014_path = '../data/image_info_test2014.json'
+
+image_info_json = json.load(open(image_info_test2014_path, 'r'))
+
+images_info = image_info_json["images"]
+
+imageIds_to_imageNames = {}
+for image in images_info:
+    id = int(image["id"])
+    name = image["file_name"]
+    imageIds_to_imageNames[id] = name
+
+with open("./data/test2014_images_ids_to_names.pkl", 'w') as fw_1:
+    pickle.dump(imageIds_to_imageNames, fw_1)
